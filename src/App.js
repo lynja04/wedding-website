9ea6e79c2a2f0c0504373bac5a9ce627e33fb644
James Lynn <j.lynn3237@gmail.com>
2019-07-26 22:11:11 -0400

    Init commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './assets/AJ06272019-CE0011-NZ6_3015.jpg';
+import './App.css';
+import './index.css';
+import styled from "styled-components";
+
+const StyledDiv = styled.div` &&& { @import url('https://fonts.googleapis.com/css?family=Sacramento');
+    font-family: 'Sacramento', cursive; color: pink; font-size: 75px;}`;
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <StyledDiv>
+          Alissa & James
+        </StyledDiv>
+      </header>
+    </div>
+  );
+}
+
+export default App;
