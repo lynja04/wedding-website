259120aa8cc93e448c62b6dee9a341a1a51e2dc0
James Lynn <j.lynn3237@gmail.com>
2019-08-05 22:58:20 -0400

    Update

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 const Navigation = () => {
+  const openGithub = () => {
+    window.open("https://github.com/lynja04/wedding-website");
+  };
   return (
     <Navbar sticky="top" bg="light" variant="light">
       <Navbar.Brand href="/home">A & J</Navbar.Brand>
@@ -10,6 +13,10 @@ const Navigation = () => {
         <Nav.Link href="/how-we-met">Our Story</Nav.Link>
         <Nav.Link href="/photos">Photos</Nav.Link>
         <Nav.Link href="/registry">Registry</Nav.Link>
+      </Nav>
+      <Nav>
+        <Nav.Link>Created by J. Lynn</Nav.Link>
+        <Nav.Link onClick={openGithub}>Source Code</Nav.Link>
       </Nav>
     </Navbar>
   );
